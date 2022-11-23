c1104e0e300baf5df80f1d78e0e0825c51eb21ff
Neze_dp17 <Neze_dp17@STHQ0124-11>
2022-11-23 18:26:27 +0400

    Add project files.

---
+++ Project2/Source.cpp
@@ -0,0 +1,28 @@
+﻿//BinarySearch Tree - İkili axtarış ağacı, nömrələrin çeşidlənmiş
+//siyahısını tez saxlamağa imkan verən məlumat strukturudur.
+
+//1. Hər ağac düyününün ən çoxu iki uşağı olduğu üçün ona ikili ağac deyilir.
+//2. Bir sayının O(log(n))zaman içinde varlığını aramak için kullanılabileceği için arama ağacı olarak adlandırılır.
+
+//Axtarma Algoritması:
+
+
+//If root == NULL
+//return NULL;
+//If number == root->data
+//return root->data;
+//If number < root->data
+//    return search(root->left)
+//    If number > root->data
+//    return search(root->right)
+
+
+//Əlavə etmə algoritması:
+
+//If node == NULL
+//return createNode(data)
+//if (data < node->data)
+//    node->left = insert(node->left, data);
+//else if (data > node->data)
+//node->right = insert(node->right, data);
+//return node;
